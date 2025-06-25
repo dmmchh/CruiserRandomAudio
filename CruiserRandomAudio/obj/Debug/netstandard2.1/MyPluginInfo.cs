f020a51d908b1690be86ddc0bb503490514576d0
ZapThe <118645969+dmmchh@users.noreply.github.com>
2025-06-25 12:47:57 +0800

    Delete CruiserRandomAudio directory

--- CruiserRandomAudio/obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace CruiserRandomAudio
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "CruiserRandomAudio";
-        public const string PLUGIN_NAME = "CruiserRandomAudio";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
